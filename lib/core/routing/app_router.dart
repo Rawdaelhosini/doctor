55bd0a7a01896f5e146aac87ebf9e2832124ce10
Rawdaelhosini <randaelhosini972@gmail.com>
2026-01-18 10:20:04 +0200

    make cubit

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,26 +1,34 @@
+import 'package:doctor/core/di/dependency_injection.dart';
 import 'package:doctor/core/routing/routes.dart';
+import 'package:doctor/features/login/logic/login_cubit.dart';
 import 'package:doctor/features/login/login_screen.dart';
 import 'package:doctor/features/onboardig/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route<dynamic> generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
     switch (settings.name) {
-    case  Routes.onBoardingScreen:
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => OnboardingScreen());
+      case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => OnboardingScreen()
-        );
-        case  Routes.loginScreen:
-        return MaterialPageRoute(
-          builder: (_) => LoginScreen()
+          builder:
+              (_) => BlocProvider(
+                create: (context) => getIt<LoginCubit>(),
+                child: LoginScreen(),
+              ),
         );
       default:
         return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(child: Text('No route defined for ${settings.name}')),
-          ),
+          builder:
+              (_) => Scaffold(
+                body: Center(
+                  child: Text('No route defined for ${settings.name}'),
+                ),
+              ),
         );
     }
   }
